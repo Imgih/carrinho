8e97675157fa4b5edd08b03873737b84d9a053ac
Your Name <you@example.com>
2024-06-19 20:27:40 -0300

    atualizacao

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,4 +1,4 @@
-import React, { Children, createContext, useContext, useState } from "react";
+import React, { Children, createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext(null)
 
@@ -10,19 +10,41 @@ export default function CartProvider ({children}) {
     const [totalPrice, setTotalPrice] = useState(40);
     const [totalCount, setTotalCount] = useState(3);
     
+    useEffect(() => {
+        const totalPrice = sum(cartItems.map(item => item.price ));
+        const totalCount = sum(cartItems.map(item => item.quantity ));
+        setTotalPrice(totalPrice);
+        setTotalCount(totalCount);
+    }, [cartItems]);
+
+    const sum = items => {
+        return items.reduce((prevValue, curValue) => prevValue + curValue, 0 );
+    };
     const removeFromCart = foodId => {
         const filteredCartItems = cartItems.filter(item => item.food.id !== foodId);
         setCartItems(filteredCartItems);
     };
 
     const changeQuantity = (cartItem, newQuantity) => {
-        
-    }
+        const {food} = cartItem;
+
+        const changedCartItem = {
+            ...cartItem,
+            quantity: newQuantity,
+            price: food.price * newQuantity,
+        };
+
+        setCartItems(
+            cartItems.map(item => (item.food.id === food.id?changedCartItem : item))
+        );
+
+    };
 
     return (
         <CartContext.Provider value={{
             cart:{items: cartItems, totalPrice, totalCount },
             removeFromCart,
+            changeQuantity,
             }}>
             {children}
         </CartContext.Provider>
